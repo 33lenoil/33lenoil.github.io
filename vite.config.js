cd33b7baf2477050aa29d25f56f5680cf4906221
33lenoil <lionelhu33@gmail.com>
2025-09-23 20:52:46 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-//   base: '/lionelhu-personal-portfolio/' // repository name
+  base: '/lionelhu-personal-portfolio/' // repository name
 })
